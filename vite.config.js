84ddd17104407e7de88dd2f1680234f0fbfa2f48
Mark Beers <mabeers@gmail.com>
2026-02-07 08:14:11 -0800

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base:"/ShapeReconstructionDemo/",
+  base:"/BlocksDemo/",
   build: {
     rollupOptions: {
       input: {
